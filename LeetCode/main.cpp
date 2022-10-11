1ef5943321821c924dc27d6c5d56a0d6b786f881
artur <arturboyd123@gmail.com>
2022-10-11 15:21:43 +0300

    Initial Commit

---
+++ LeetCode/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  LeetCode
+//
+//  Created by artur boyd on 11/10/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
