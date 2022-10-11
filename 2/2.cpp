9d925353f438ed4011ece8795f25a273fdde362f
artur <arturboyd123@gmail.com>
2022-10-11 15:25:17 +0300

    sdaasd

---
+++ 2/2.cpp
@@ -0,0 +1,9 @@
+//
+//  2.cpp
+//  LeetCode
+//
+//  Created by artur boyd on 11/10/2022.
+//
+
+#include <stdio.h>
+//sadasd
---
+++ 3/3.cpp
@@ -0,0 +1,8 @@
+//
+//  3.cpp
+//  LeetCode
+//
+//  Created by artur boyd on 11/10/2022.
+//
+
+#include <stdio.h>
